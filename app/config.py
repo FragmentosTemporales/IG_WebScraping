cf639a1aa31ebc487e6ae163ad757f636ffe3041
FragmentosTemporales <cristian.rivera3284@gmail.com>
2024-07-11 15:05:28 -0400

    Creación documento example.env

--- app/config.py
+++ app/config.py
@@ -4,9 +4,6 @@ class Settings(BaseSettings):
     navegador : str
     dir_descargas : str = r'C:\Users\Usuario\OneDrive - Dominion Global\Escritorio\AutoBI\app\downloads'
 
-    user_bi : str
-    pw_bi : str
-
     user_ig : str
     pw_ig : str
 
